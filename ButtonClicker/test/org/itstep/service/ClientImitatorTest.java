7f213c47d5da398b4325e726b9586b8218e6e9be
Alex <ignatenko2207@gmail.com>
2018-01-25 19:26:18 +0200

    Prj with selenium jars

---
+++ ButtonClicker/test/org/itstep/service/ClientImitatorTest.java
@@ -0,0 +1,32 @@
+package org.itstep.service;
+
+import static org.junit.Assert.*;
+
+import org.itstep.model.Account;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class ClientImitatorTest {
+
+	@Test
+	public void testLoginAccount() {
+
+		Account acc = new Account("Alex", "Ignatenko", "ignatenko2207@gmail.com", "alexsuperpuper2207");
+
+		ClientImitator imitator = new ClientImitator();
+
+		WebDriver driver = imitator.registerAmazonAccount(acc);
+		
+		Timer.waitSeconds(5);
+		
+//		assertTrue(driver.getPageSource().contains("Helo, "));
+		
+		driver = imitator.addItemToWL(driver, "B077N43PST");
+		
+		assertTrue(driver.getPageSource().contains(""));
+		
+		driver.quit();
+	
+	}
+
+}
